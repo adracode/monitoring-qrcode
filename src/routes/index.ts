cd3833cd65625e3cc2386e6b215c40571f03d3ca
Benjamin <benjamin.lecesne.blc@gmail.com>
2023-02-06 23:18:46 +0100

    continue: prise en compte des nouvelles routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import sensors from "./sensors";
 import qrcode from "./qrcode-generator";
+import configSensors from "./config-sensors"
+import configNames from "./config-names"
 //import dev from "./dev";
 import {getPathPage} from "../utils/path";
 
@@ -8,8 +10,14 @@ const router = express.Router();
 
 router.use("/", express.static(getPathPage("sensors"), {extensions:['html'], index: "sensor.html"}))
 router.use("/sensors", sensors);
-router.use("/qrcode", express.static(getPathPage("qrcode"), {extensions: ['html']}))
-router.use("/qrcode", qrcode)
+router.use("/qrcode", express.static(getPathPage("qrcode"), {extensions: ['html']}));
+router.use("/qrcode", qrcode);
+router.use("/authentication", express.static(getPathPage("authentication"), {extensions: ['html']}));
+router.use("/config-sensors", express.static(getPathPage("config-sensors"), {extensions: ['html'], index: "sensors.html"}));
+router.use("/config-sensors", configSensors);
+router.use("/config-names", express.static(getPathPage("config-names"), {extensions: ['html'], index: "names.html"}));
+router.use("/config-names", configNames);
+
 
 //router.use("/dev", express.static(getPathPage("dev")))
 //router.use("/dev", dev);
