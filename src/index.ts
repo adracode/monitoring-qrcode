1caf90ab1f2fd54f859fd031225484efa037698f
Antoine LEFEBVRE <48187865+adracode@users.noreply.github.com>
2023-03-09 23:44:44 +0100

    continue: Serveur HTTPS

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,15 @@ import app from "./app"
 import { ConfigurationManager, SensorManager } from "./services/data-management";
 import { TokenManager } from "./services/token";
 import { Sensor } from "./services/sensor";
+import * as https from "https";
+import fs from "fs";
 
 const port = 3000;
+
+const server = https.createServer({
+    key: fs.readFileSync(process.env.PRIVATE_KEY_PATH as string),
+    cert: fs.readFileSync(process.env.CERTIFICATE_PATH as string)
+}, app);
 
 (async () => {
     console.log("Initialisation de la connexion aux bases de données")
@@ -13,7 +20,7 @@ const port = 3000;
     console.log("Préparation de la gestion des données")
     await SensorManager.getInstance().init();
     await ConfigurationManager.getInstance().init();
-    app.listen(port, () => console.log(`Serveur en écoute sur le port ${port}`));
+    server.listen(port, () => console.log(`Serveur en écoute sur le port ${port}`));
     process.on('exit', () => ConfigurationManager.getInstance().close());
 })();
 
